5232124fb37bdd8243bc0158d15a002bfccaf02f
FabricePaka <fabricepaka@le-campus-numerique.fr>
2024-12-02 16:51:14 +0100

    lundi soir sauvegarde

--- dockerfile
+++ dockerfile
@@ -1,11 +1,12 @@
 # syntax=docker/dockerfile:1
 
 FROM	python:3.8-slim-buster
-WORKDIR /python_docker
+WORKDIR .
 
 COPY	requirements.txt requirements.txt
-RUN     pip3 install -r requirements.txt
+RUN     pip install -r requirements.txt
 
 COPY . .
+EXPOSE 5000
 
-CMD ["python3", "-m", "flask", "run", "--host=0.0.0.0" ]
+CMD ["python3", "-m", "flask", "run", "-p=5000", "--host=0.0.0.0"]
--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 from flask import Flask, render_template,request
+import os
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploads/'
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     return render_template('index.html')
 
-# @app.route("/model",methods=['GET', 'POST'])
-# def about():
-#     return render_template('model.html')
-
 @app.route('/model', methods=['GET', 'POST'])
 def submit():
-    # Récupérer les données du formulaire
-    img = request.form.get('img')
-    prompt = request.form.get('Prompt')
-    
+    if request.method == 'POST':
+        # Récupérer les données du formulaire
+        img = request.files.get('img')
+        prompt = request.form.get('Prompt')
+        if not img or img.filename == '':
+            return "Aucun fichier reçu ou fichier non valide", 400  # Vérifiez que le fichier est présent
+
+
+        image_path = os.path.join(app.config['UPLOAD_FOLDER'], img.filename)
+        img.save(image_path)
+        return render_template('model.html', image_url=image_path,  prompt=prompt)
     # Traiter les données
-    return render_template('model.html', prompt=prompt)
+    return render_template('model.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
