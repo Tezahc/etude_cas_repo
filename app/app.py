ecef6aa9659782d7ec58906504d1defa6c4dbf3e
rodolphe. <rodolphe.robert@le-campus-numerique.fr>
2024-12-03 13:33:51 +0100

    feat : edit docker compose and new route

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, Response
+from config import app, db, api
 
 app = Flask(__name__)
 
@@ -10,7 +11,26 @@ def home():
 def about():
     return render_template('about.html')
 
+class Upload(db.Model):
+    _tablename_ = 'upload'
 
+    id = db.Column(db.Integer, primary_key=True)
+    img = db.Column(db.Text, unique=True, nullable=False)
+    filename = db.Column(db.String(50), nullable=False)
+    mimetype = db.Column(db.Text, nullable=False)
+
+@app.route('/model', methods=['POST', 'GET'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['files']
+
+        if not file:
+            return ({"error: No image uploaded"}, 400)
+    
+    mimetype = file.mimetype
+    upload = Upload(img=file.read(), filename=file.filename, mimetype=mimetype)
+    db.session.add(upload)
+    db.session.commit()
 
 
 
